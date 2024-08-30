468d9544dea06bb04f4ca6747d1aa2aab615bbd5
rirefat <rirefat.official@gmail.com>
2024-08-30 08:32:27 +0600

    implement theme changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    }
+    else {
+      setTheme('light');
+    }
+  }, [])
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div
+      className="bg-gray-100 dark:bg-slate-900 h-screen flex flex-col justify-center items-center"
+    >
+      <h1 className="text-5xl my-8 dark:text-gray-100">TailWind CSS Theme Changer</h1>
 
-export default App
+      <button
+        onClick={handleThemeSwitch}
+        className="bg-slate-800 dark:bg-slate-600 dark:text-gray-100 hover:bg-slate-900 dark:hover:bg-slate-500 text-gray-100 px-8 py-2 rounded-lg shadow-md cursor-pointer"
+      >
+        Dark Mode
+      </button>
+    </div>
+  );
+};
+
+export default App;
